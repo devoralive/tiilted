e80dddff215adef64057dc60cd7e1ccd1e58f196
Jeremy Berthelier <jeremyberthelier@hotmail.fr>
2018-03-23 13:33:14 +0100

    Add Knex & mysql package

--- app/index.js
+++ app/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './components/App.jsx';
+
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'root',
+    password : 'tiilted',
+    database : 'tiilted'
+  }
+});
+
+knex.select('name').from('user').map(function(row) {
+  console.log(row)
+})
 
 // Rendering
 ReactDOM.render( <App />, document.getElementById('root') );
